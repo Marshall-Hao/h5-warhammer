40a285b35f7642e4990bda72af1aa8e7e9aa47ac
Agnes Selvita <a.selvita@gmail.com>
2022-03-11 09:20:48 +0800

    test dokku

--- src/services/ahoy.js
+++ src/services/ahoy.js
@@ -1,7 +1,7 @@
 import ahoy from "ahoy.js";
 
 ahoy.configure({
-  urlPrefix: "https://wh-staging.papercranetech.cn",
+  urlPrefix: "https://api-staging.h5.games-workshop-china.com",
   visitDuration: 30 //4 * 60, // 4 hours
 });
 
